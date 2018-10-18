793582d85a775a391b322a1657d1c8048329f6a9
unknown <amir.mehrabi.j@gmail.com>
2018-10-19 00:55:11 +0330

    new example(s) added

---
+++ faceDetection.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+import numpy as np
+cap = cv.VideoCapture(0)
+frame = cap.read()
+
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
+
+while(1):
+
+
+    ret ,frame = cap.read()
+
+    if ret == True:
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in faces:
+            cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            roi_color = frame[y:y + h, x:x + w]
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            for (ex, ey, ew, eh) in eyes:
+                cv.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+        cv.imshow('img2',frame)
+
+
+        k = cv.waitKey(60) & 0xff
+        if k == 27:
+            break
+
+    else:
+        break
+cv.destroyAllWindows()
+cap.release()
